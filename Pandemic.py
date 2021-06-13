5f9dba31b2c178191157ac584b5ebeb94b7d9d66
qrhysse <chrisharrison014@gmail.com>
2021-06-13 07:26:16 -0400

    Update Pandemic.py

--- Pandemic.py
+++ Pandemic.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 #The number of cards that are flipped during infect cities step
 infection_tracker = 2
@@ -36,15 +37,31 @@ class Deck:
         self.discard_list = [card for card in card_df[card_df.location == 'Discard'].unique_name]
 
 #Change location in dataframe to 'discard,' remove card from deck list and add to discard list
-    def discard_top(self, card_name):
+    def discard_top(self, card_input):
+        card_name = self.find_best_match(card_input)
         if self.cards.at[card_name, 'forsaken'] == True:
             print('Move the card to the \"Game End\" area and infect a new city.')
+            self.cards.at[card_name, 'location'] = 'Game End'
+            del self.deck_list[0]
+            for slot in self.deck_list:
+                slot.remove(card_name)
         else:
             self.cards.at[card_name, 'location'] = 'Discard'
             del self.deck_list[0]
             for slot in self.deck_list:
                 slot.remove(card_name)
             self.discard_list.insert(0, card_name)
+            
+    def find_best_match(self, card_name):
+        match_list = []
+        pattern = re.escape(card_name) + '[0-9]+'
+        for card in self.deck_list[0]:
+            match = re.search(pattern, card)
+            if match: 
+                match_list.append(card)
+        try: return match_list[0]
+        except: 
+            print('That can\'t be on top!')
         
 #Move cards from discard to deck. Add those cards to possible cards in deck list 
 #and remove from discard list
@@ -53,7 +70,6 @@ class Deck:
             self.cards.at[card, 'location'] = 'Deck'
             self.deck_list.insert(0, self.discard_list)
         self.discard_list = []
-#print(infection_cards)
 
 
 
@@ -70,12 +86,15 @@ print(infection_cards)
 print(infection_deck.discard_list)
 
 #Discard two cards to mimic infect cities step at end of turn
-infection_deck.discard_top('New York3')
-infection_deck.discard_top('Osaka9')
+infection_deck.discard_top('New York')
+infection_deck.discard_top('Osaka')
 #Print the card Sataframe, deck list, and discard list to verify discard functionality
 print(infection_cards)
 print(infection_deck.deck_list)
 print(infection_deck.discard_list)
+
+#Test discarding 0 population city
+infection_deck.discard_top('Cairo')
 
 #Test of epidemic funtion
 infection_deck.epidemic()
@@ -84,4 +103,4 @@ print(infection_deck.deck_list)
 print(infection_deck.discard_list)
 
 #Test discarding 0 population city
-infection_deck.discard_top('Cairo0')
+#infection_deck.discard_top('Cairo')
